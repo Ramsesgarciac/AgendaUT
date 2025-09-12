cda6d4366787000777e09cf995ecfd71f10150f2
Ramsesgarciac <garciacuevasramses101@gmail.com>
2025-09-12 10:35:46 -0600

    punto de retorno

--- src/comentarios/comentarios.module.ts
+++ src/comentarios/comentarios.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ComentariosService } from './comentarios.service';
 import { ComentariosController } from './comentarios.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Comentarios } from './entities/comentario.entity';
 
 @Module({
+  imports: [
+        TypeOrmModule.forFeature([Comentarios]), // Esto es crucial
+      ],
   controllers: [ComentariosController],
   providers: [ComentariosService],
+  exports: [ComentariosService]
 })
 export class ComentariosModule {}
